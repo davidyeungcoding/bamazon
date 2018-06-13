bb0b2104f8b2e255a3d7a9902eaeabbec6eea244
yeungpirate <yeungpirate@gmail.com>
2018-06-13 09:24:56 -0700

    bare bones setup for table and application

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,10 +5,53 @@
 var mysql = require('mysql');
 var inquirer = require ('inquirer');
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: '',
+    database: 'bamazon'
+});
+
 // ===============
 // || FUNCTIONS ||
 // ===============
 
+function landingScreen() {
+    connection.query(`SELECT * FROM products`, function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(`Item ID: ${res[i].item_id} || Product Name: ${res[i].product_name} || Department: ${res[i].department_name} || Price: ${res[i].price} || Stock: ${res[i].stock_quantity}`);
+        }
+        purchase();
+    })
+};
+
+function purchase() {
+    inquire.prompt(
+        {
+            name: 'item',
+            type: 'input',
+            message: 'Please input the Item ID for the item you would like to purchase.'
+            // add validation for valid ID entered
+        },
+        {
+            name: 'quantity',
+            type: 'input',
+            message: 'How many would you like to order?'
+            // add validation for number and qunatity under current stock
+        }
+    ).then(function() {
+
+    })
+};
+
 // ================
 // || APLICATION ||
 // ================
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`Connection established with user ID: ${connection.threadId}`);
+    landingScreen();
+});
