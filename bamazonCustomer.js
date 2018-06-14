fd87046c6d27e697efed9511ff71255e7e5ad80e
yeungpirate <yeungpirate@gmail.com>
2018-06-13 21:10:06 -0700

    item purchase and quantitiy update set

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,22 +28,55 @@ function landingScreen() {
 };
 
 function purchase() {
-    inquire.prompt(
+    inquirer.prompt(
         {
             name: 'item',
             type: 'input',
-            message: 'Please input the Item ID for the item you would like to purchase.'
-            // add validation for valid ID entered
+            message: 'Please input the Item ID for the item you would like to purchase.',
+            validate: function(value) {
+                if (isNaN(value) === false && value > 0) { // might look into setting a limit of the length of our item list
+                    return true;
+                }
+                if (value === 0) { // migt not work because of the return in previous condition
+                    console.log('Cancelling order.')
+                }
+                return false;
+            }
         },
         {
             name: 'quantity',
             type: 'input',
-            message: 'How many would you like to order?'
-            // add validation for number and qunatity under current stock
+            message: 'How many would you like to order?',
+            validate: function(value) {
+                if (isNaN(value) === false && value > 0) { // might look into setting a limit of the length of our item list
+                    return true;
+                }
+                if (value === 0) { // migt not work because of the return in previous condition
+                    console.log('Cancelling order.')
+                }
+                return false;
+            }
         }
-    ).then(function() {
+    ).then(function(itemSelect) {
+        // function for updating the DB with the new stock after purchase
+        if (itemSelect.item.stock_quantity > 0) {
+            connection.query('UPDATE bamazon SET stock_quantity = stock_quantity - 1 WHERE ?', {item_id: itemSelect.item}), function(err, res) {
+                if (err) throw err;
+            }
+        }
+        else {
+            console.log('Insufficient quantity!');
+        }
+        total();
+        // connection.query('SELECT * FROM bamazon WHERE ?', {item_id: itemSelect.item}, function(err,res) {
+        //     if (err) throw err;
+            
+        // })
+    });
+};
 
-    })
+function total() {
+
 };
 
 // ================
--- bamazonInventoryDB.sql
+++ bamazonInventoryDB.sql
@@ -5,9 +5,17 @@ USE bamazon;
 
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(25) NOT NULL,
-    department_name VARCHAR(25) NOT NULL,
-    price INT NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
     stock_quantity INT NOT NULL,
     PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('System 76 Gazelle', 'Computers', 899.99, 10), 
+('TCL 55-inch TV', 'Electronics', 599.99, 5), ('Identity Crisis', 'Books', 19.99, 7), 
+('Batman: White Knight', 'Books', 15.99, 0), ('Aldo Mens Hermond', 'Shoes', 42.99, 5), 
+('Skin & Earth', 'Music', 9.49, 10), ('Firefly Season 1', 'Movies & TV', 15.99, 15), 
+('The Rocketeer', 'Movies & TV', 15.99, 15), ('Toshiba Microwave', 'Home & Kitchen', 89.99, 2), 
+('Eyourlife Military Folding Shovel', 'Outdoor', 9.99, 4)
